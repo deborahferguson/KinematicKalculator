c02152ab5d3db0db1e43b6704b5ec757f8ef4027
Deborah Ferguson <deborah.l.ferguson@gmail.com>
2015-07-13 22:17:19 -0400

    Created Rotational Solutions activity. Copied linear solutions activity but changed variables to rotational variables. Added 16 new equations for rotational solution walk through.

--- app/src/main/java/com/developer/debbie/kinematickalculator/Radial.java
+++ app/src/main/java/com/developer/debbie/kinematickalculator/Radial.java
@@ -69,11 +69,30 @@ public class Radial extends ActionBarActivity implements View.OnClickListener{
             String f = finalVel.getText().toString();
             String t = time.getText().toString();
 
-            String result = Equations(a, d, i, f, t);
-            if(result.contains("NaN")){
-                result = "These values are not possible";
+            double[] result = Equations(a, d, i, f, t);
+            //if(result.contains("NaN")){
+            //    result = "These values are not possible";
+            //}
+            //ErrorMessage.setText(result);
+            boolean error = false;
+            for(int j=0; j<6; j++){
+                if(Double.toString(result[j]).equals("NaN")){
+                    error=true;
+                    break;
+                }
             }
-            ErrorMessage.setText(result);
+            if(error){
+                result[5]=1;
+            }
+            if(result[5]==1){
+                ErrorMessage.setText("These values are not possible");
+            }
+            else if(result[5]==2){
+                ErrorMessage.setText("Insufficient information");
+            }
+            else {
+                startSolutions(result);
+            }
         }
 
         if(v.getId()==R.id.ResetButton){
@@ -91,67 +110,165 @@ public class Radial extends ActionBarActivity implements View.OnClickListener{
             finalVel.setText("");
             time.setText("");
         }
+
     }
 
-    public String Equations(String a, String d, String i, String f, String t){
-        String result = "";
+    public void startSolutions(double[] values){
+        Intent intent = new Intent(this, RotationalSolutions.class);
+        intent.putExtra("acceleration", values[0]);
+        intent.putExtra("distance", values[1]);
+        intent.putExtra("initial velocity", values[2]);
+        intent.putExtra("final velocity", values[3]);
+        intent.putExtra("time", values[4]);
+        intent.putExtra("agiven", values[6]);
+        intent.putExtra("dgiven", values[7]);
+        intent.putExtra("igiven", values[8]);
+        intent.putExtra("fgiven", values[9]);
+        intent.putExtra("tgiven", values[10]);
+        intent.putExtra("first equation", values[11]);
+        intent.putExtra("second equation", values[12]);
+
+        startActivity(intent);
+    }
+
+    public double[] Equations(String a, String d, String i, String f, String t){
+        //equation1a - 1
+        //equation1d - 2
+        //equation1i - 3
+        //equation1t - 4
+        //equation2a - 5
+        //equation2d - 6
+        //equation2f - 7
+        //equation2i - 8
+        //equation3a - 9
+        //equation3f - 10
+        //equation3i - 11
+        //equation3t - 12
+        //equation4d - 13
+        //equation4f - 14
+        //equation4i - 15
+        //equation4t - 16
+        double acceleration=0;
+        double distance=0;
+        double initialvel=0;
+        double finalvel=0;
+        double time=0;
+        double error=0;//will be 0 for no error, 1 for values not possible, 2 for not enough information
+        double agiven = 0;
+        double dgiven = 0;
+        double igiven = 0;
+        double fgiven = 0;
+        double tgiven = 0;
+        double firstEquation = 0;
+        double secondEquation = 0;
+
         if(a.equals("") && !d.equals("") && !i.equals("") && !f.equals("") && !t.equals("")){
             double dist = new Double(d);
             double init = new Double(i);
             double fin = new Double(f);
             double tim = new Double(t);
+            distance = dist;
+            initialvel = init;
+            finalvel = fin;
+            time = tim;
+            dgiven = 1;
+            igiven = 1;
+            fgiven = 1;
+            tgiven = 1;
 
             double acc = (fin-init)/tim;
             double acc2 = (dist-init*tim)*2/(tim*tim);
             if(Math.round(acc*10000)==Math.round(acc2*10000)){
-                result = "acceleration = "+acc;
+                //result = "acceleration = "+acc;
+                acceleration=acc;
+                firstEquation = 9;
             }
             else{
-                result = "These values are not possible ";
+                //result = "These values are not possible ";
+                error=1;
             }
         }
         else if(a.equals("") && d.equals("") && !i.equals("") && !f.equals("") && !t.equals("")){
             double init = new Double(i);
             double fin = new Double(f);
             double tim = new Double(t);
+            initialvel = init;
+            finalvel = fin;
+            time = tim;
+            igiven = 1;
+            fgiven = 1;
+            tgiven = 1;
 
             double acc = (fin-init)/tim;
+            firstEquation = 9;
             double dist = (init+fin)*tim/2.0;
+            secondEquation = 13;
 
-            result = "acceleration = "+acc+" and distance = "+dist;
+            //result = "acceleration = "+acc+" and distance = "+dist;
+            acceleration = acc;
+            distance = dist;
         }
         else if(a.equals("") && !d.equals("") && i.equals("") && !f.equals("") && !t.equals("")){
             double dist = new Double(d);
             double fin = new Double(f);
             double tim = new Double(t);
+            distance = dist;
+            finalvel = fin;
+            time = tim;
+            dgiven = 1;
+            fgiven = 1;
+            tgiven = 1;
 
             double init = dist*2/tim-fin;
+            firstEquation = 15;
             double acc = (fin-init)/tim;
+            secondEquation = 9;
 
-            result = "acceleration = "+acc+" and initial velocity = "+init;
+            //result = "acceleration = "+acc+" and initial velocity = "+init;
+            acceleration = acc;
+            initialvel = init;
         }
         else if(a.equals("") && !d.equals("") && !i.equals("") && f.equals("") && !t.equals("")){
             double dist = new Double(d);
             double init = new Double(i);
             double tim = new Double(t);
+            distance = dist;
+            initialvel = init;
+            time = tim;
+            dgiven = 1;
+            igiven = 1;
+            tgiven = 1;
 
             double acc = (dist-init*tim)*2/(tim*tim);
+            firstEquation = 1;
             double fin = dist*2/tim-init;
-
-            result = "acceleration = "+acc+" and final velocity = "+fin;
+            secondEquation = 14;
+            //result = "acceleration = "+acc+" and final velocity = "+fin;
+            acceleration = acc;
+            finalvel = fin;
         }
         else if(a.equals("") && !d.equals("") && !i.equals("") && !f.equals("") && t.equals("")){
             double dist = new Double(d);
             double init = new Double(i);
             double fin = new Double(f);
+            distance = dist;
+            initialvel = init;
+            finalvel = fin;
+            dgiven = 1;
+            igiven = 1;
+            fgiven = 1;
 
             double acc = (fin*fin-init*init)/(2*dist);
+            firstEquation = 5;
             double tim = dist*2/(init+fin);
+            secondEquation = 16;
             if(tim<0){
                 tim = tim*-1;
             }
 
-            result = "acceleration = "+acc+" and time = "+tim;
+            //result = "acceleration = "+acc+" and time = "+tim;
+            acceleration = acc;
+            time = tim;
         }
 
         else if(!a.equals("") && d.equals("") && !i.equals("") && !f.equals("") && !t.equals("")){
@@ -159,49 +276,92 @@ public class Radial extends ActionBarActivity implements View.OnClickListener{
             double init = new Double(i);
             double fin = new Double(f);
             double tim = new Double(t);
+            acceleration = acc;
+            initialvel = init;
+            finalvel = fin;
+            time = tim;
+            agiven = 1;
+            igiven = 1;
+            fgiven = 1;
+            tgiven = 1;
 
             double dist = init*tim + acc*tim*tim/2;
             double dist2 = (init+fin)*tim/2;
 
             if(Math.round(dist*10000) == Math.round(dist2*10000)){
-                result = "distance = "+dist;
+                //result = "distance = "+dist;
+                distance = dist;
+                firstEquation = 2;
             }
             else{
-                result = "Those values are not possible";
+                //result = "Those values are not possible";
+                error = 1;
             }
         }
         else if(!a.equals("") && d.equals("") && i.equals("") && !f.equals("") && !t.equals("")){
             double acc = new Double(a);
             double fin = new Double(f);
             double tim = new Double(t);
+            acceleration = acc;
+            finalvel = fin;
+            time = tim;
+            agiven = 1;
+            fgiven = 1;
+            tgiven = 1;
 
             double init = fin-acc*tim;
-            double dist = fin*tim-(1/2)*acc*tim*tim;
+            firstEquation = 11;
+            //this equation seemed to be wrong....
+            //check this
+            //keep checking it
             //double dist = fin*tim-(-2)*(acc*tim)*(acc*tim);
+            double dist = fin*tim-(1/2)*acc*tim*tim;
+            secondEquation = 2;
 
-            result = "distance = "+dist+" and initial velocity = "+init;
+            //result = "distance = "+dist+" and initial velocity = "+init;
+            distance = dist;
+            initialvel = init;
         }
         else if(!a.equals("") && d.equals("") && !i.equals("") && f.equals("") && !t.equals("")){
             double acc = new Double(a);
             double init = new Double(i);
             double tim = new Double(t);
+            acceleration = acc;
+            initialvel = init;
+            time = tim;
+            agiven = 1;
+            igiven = 1;
+            tgiven = 1;
 
             double dist = init*tim+acc*tim*tim/2;
+            firstEquation = 2;
             double fin = init+acc*tim;
-
-            result = "distance = "+dist+" and final velocity = "+fin;
+            secondEquation = 10;
+            //result = "distance = "+dist+" and final velocity = "+fin;
+            distance = dist;
+            finalvel = fin;
         }
         else if(!a.equals("") && d.equals("") && !i.equals("") && !f.equals("") && t.equals("")){
             double acc = new Double(a);
             double init = new Double(i);
             double fin = new Double(f);
+            acceleration = acc;
+            initialvel = init;
+            finalvel = fin;
+            agiven = 1;
+            igiven = 1;
+            fgiven = 1;
 
             double dist = (fin*fin-init*init)/(2*acc);
+            firstEquation = 6;
             double tim = (fin-init)/acc;
+            secondEquation = 12;
             if(tim<0){
                 tim=tim*-1;
             }
-            result = "distance = "+dist+" and time = "+tim;
+            //result = "distance = "+dist+" and time = "+tim;
+            distance = dist;
+            time = tim;
         }
 
         else if(!a.equals("") && !d.equals("") && i.equals("") && !f.equals("") && !t.equals("")){
@@ -209,40 +369,73 @@ public class Radial extends ActionBarActivity implements View.OnClickListener{
             double dist = new Double(d);
             double fin = new Double(f);
             double tim = new Double(t);
+            acceleration = acc;
+            distance = dist;
+            finalvel = fin;
+            time = tim;
+            agiven = 1;
+            dgiven = 1;
+            fgiven = 1;
+            tgiven = 1;
 
             double init = fin-acc*tim;
             double init2 = dist*2*tim-fin;
 
             if(Math.round(init*10000) == Math.round(init2*10000)){
-                result = "initial velocity = "+init;
+                //result = "initial velocity = "+init;
+                initialvel = init;
+                firstEquation = 11;
             }
             else {
-                result = "Those values are not possible";
+                //result = "Those values are not possible";
+                error = 1;
             }
         }
         else if(!a.equals("") && !d.equals("") && i.equals("") && f.equals("") && !t.equals("")){
             double acc = new Double(a);
             double dist = new Double(d);
             double tim = new Double(t);
+            acceleration = acc;
+            distance = dist;
+            time = tim;
+            agiven = 1;
+            dgiven = 1;
+            tgiven = 1;
 
             double init = (dist-acc*tim*tim/2)/tim;
+            firstEquation = 3;
+            //double check this
+            //double check this
             //double fin = (dist+(acc*tim)*(acc*tim)/2)/tim;
             double fin = (dist+(acc*tim*tim)/2)/tim;
+            secondEquation = 10;
 
-            result = "initial velocity = "+init+" and final velocity = "+fin;
+            //result = "initial velocity = "+init+" and final velocity = "+fin;
+            initialvel = init;
+            finalvel = fin;
         }
         else if(!a.equals("") && !d.equals("") && i.equals("") && !f.equals("") && t.equals("")){
             double acc = new Double(a);
             double dist = new Double(d);
             double fin = new Double(f);
+            acceleration = acc;
+            distance = dist;
+            finalvel = fin;
+            agiven = 1;
+            dgiven = 1;
+            fgiven = 1;
 
             double init = Math.sqrt(fin*fin-2*acc*dist);
+            firstEquation = 8;
             double tim = (fin-init)/acc;
+            secondEquation = 12;
             if(tim<0){
                 tim=tim*-1;
             }
 
-            result = "initial velocity = "+init+" and time = "+tim;
+            //result = "initial velocity = "+init+" and time = "+tim;
+            initialvel = init;
+            time = tim;
         }
 
         else if(!a.equals("") && !d.equals("") && !i.equals("") && f.equals("") && !t.equals("")){
@@ -250,24 +443,43 @@ public class Radial extends ActionBarActivity implements View.OnClickListener{
             double dist = new Double(d);
             double init = new Double(i);
             double tim = new Double(t);
+            acceleration = acc;
+            distance = dist;
+            initialvel = init;
+            time = tim;
+            agiven = 1;
+            dgiven = 1;
+            igiven = 1;
+            tgiven = 1;
 
             double fin = init+acc*tim;
             double fin2 = (dist*2/tim)-init;
 
             if(Math.round(fin*10000) == Math.round(fin2*10000)){
-                result = "final velocity = "+fin;
+                //result = "final velocity = "+fin;
+                finalvel = fin;
+                firstEquation = 10;
             }
             else{
-                result = "Those values are not possible";
+                //result = "Those values are not possible";
+                error = 1;
             }
         }
         else if(!a.equals("") && !d.equals("") && !i.equals("") && f.equals("") && t.equals("")){
             double acc = new Double(a);
             double dist = new Double(d);
             double init = new Double(i);
+            acceleration = acc;
+            distance = dist;
+            initialvel = init;
+            agiven = 1;
+            dgiven = 1;
+            igiven = 1;
 
             double fin = Math.sqrt(init*init+2*acc*dist);
+            firstEquation = 7;
             double tim = (fin-init)/acc;
+            secondEquation = 12;
 
             double dist2 = init*tim+acc*tim*tim/2;
             Log.i("tag", "" + dist2);
@@ -279,7 +491,9 @@ public class Radial extends ActionBarActivity implements View.OnClickListener{
                 tim=tim*-1;
             }
 
-            result = "final velocity = "+fin+" and time = "+tim;
+            //result = "final velocity = "+fin+" and time = "+tim;
+            finalvel = fin;
+            time = tim;
         }
 
         else if(!a.equals("") && !d.equals("") && !i.equals("") && !f.equals("") && t.equals("")){
@@ -287,6 +501,14 @@ public class Radial extends ActionBarActivity implements View.OnClickListener{
             double dist = new Double(d);
             double init = new Double(i);
             double fin = new Double(f);
+            acceleration = acc;
+            distance = dist;
+            initialvel = init;
+            finalvel = fin;
+            agiven = 1;
+            dgiven = 1;
+            igiven = 1;
+            fgiven = 1;
 
             double tim = (fin-init)/acc;
             double tim2 = dist*2/(init+fin);
@@ -295,18 +517,37 @@ public class Radial extends ActionBarActivity implements View.OnClickListener{
                 tim=-1*tim;
             }
             if(Math.round(tim*10000) == Math.round(tim2*10000)){
-                result = "time = "+tim;
+                //result = "time = "+tim;
+                time = tim;
+                firstEquation = 12;
             }
             else{
-                result = "Those values are not possible";
+                //result = "Those values are not possible";
+                error = 1;
             }
         }
 
         else{
-            result = "Please only leave 1 or 2 spaces blank";
+            //result = "Please only leave 1 or 2 spaces blank";
+            error = 2;
         }
 
-        return result;
+        //return result;
+        double[] returnVal = new double[13];
+        returnVal[0]=acceleration;
+        returnVal[1]=distance;
+        returnVal[2]=initialvel;
+        returnVal[3]=finalvel;
+        returnVal[4]=time;
+        returnVal[5]=error;
+        returnVal[6]=agiven;
+        returnVal[7]=dgiven;
+        returnVal[8]=igiven;
+        returnVal[9]=fgiven;
+        returnVal[10]=tgiven;
+        returnVal[11]=firstEquation;
+        returnVal[12]=secondEquation;
+        return returnVal;
     }
 
 }
---
+++ app/src/main/java/com/developer/debbie/kinematickalculator/RotationalSolutions.java
@@ -0,0 +1,195 @@
+package com.developer.debbie.kinematickalculator;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class RotationalSolutions extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+
+        double acceleration = intent.getDoubleExtra("acceleration",0.0);
+        double distance = intent.getDoubleExtra("distance",0.0);
+        double iVelocity = intent.getDoubleExtra("initial velocity",0.0);
+        double fVelocity = intent.getDoubleExtra("final velocity",0.0);
+        double time = intent.getDoubleExtra("time",0.0);
+
+        //rounding to keep 2 decimal places
+        acceleration = ((double)(Math.round(acceleration*100)))/100.0;
+        distance = ((double)(Math.round(distance*100)))/100.0;
+        iVelocity = ((double)(Math.round(iVelocity*100)))/100.0;
+        fVelocity = ((double)(Math.round(fVelocity*100)))/100.0;
+        time = ((double)(Math.round(time*100)))/100.0;
+
+        //keeping track of which equations are used
+        double firstEquation = intent.getDoubleExtra("first equation", 0.0);
+        double secondEquation = intent.getDoubleExtra("second equation", 0.0);
+
+        setContentView(R.layout.activity_rotational_solutions);
+
+        TextView AccelerationValue = (TextView) findViewById(R.id.AccelerationValue);
+        TextView DistanceValue = (TextView) findViewById(R.id.DistanceValue);
+        TextView InitialVelocityValue = (TextView) findViewById(R.id.InitialVelocityValue);
+        TextView FinalVelocityValue = (TextView) findViewById(R.id.FinalVelocityValue);
+        TextView TimeValue = (TextView) findViewById(R.id.TimeValue);
+
+        if(intent.getDoubleExtra("agiven",0.0)==0.0)
+            AccelerationValue.setTextColor(Color.GREEN);
+        if(intent.getDoubleExtra("dgiven",0.0)==0.0)
+            DistanceValue.setTextColor(Color.GREEN);
+        if(intent.getDoubleExtra("igiven",0.0)==0.0)
+            InitialVelocityValue.setTextColor(Color.GREEN);
+        if(intent.getDoubleExtra("fgiven",0.0)==0.0)
+            FinalVelocityValue.setTextColor(Color.GREEN);
+        if(intent.getDoubleExtra("tgiven",0.0)==0.0)
+            TimeValue.setTextColor(Color.GREEN);
+
+        AccelerationValue.setText(Double.toString(acceleration));
+        DistanceValue.setText(Double.toString(distance));
+        InitialVelocityValue.setText(Double.toString(iVelocity));
+        FinalVelocityValue.setText(Double.toString(fVelocity));
+        TimeValue.setText(Double.toString(time));
+
+        ImageView Equation1 = (ImageView) findViewById(R.id.FirstEquationPicture);
+        ImageView Equation2 = (ImageView) findViewById(R.id.SecondEquationPicture);
+
+        switch ((int)firstEquation) {
+            case 1:
+                Equation1.setImageResource(R.drawable.rotationalequation1a);
+                break;
+            case 2:
+                Equation1.setImageResource(R.drawable.rotationalequation1d);
+                break;
+            case 3:
+                Equation1.setImageResource(R.drawable.rotationalequation1i);
+                break;
+            case 4:
+                Equation1.setImageResource(R.drawable.rotationalequation1t);
+                break;
+            case 5:
+                Equation1.setImageResource(R.drawable.rotationalequation2a);
+                break;
+            case 6:
+                Equation1.setImageResource(R.drawable.rotationalequation2d);
+                break;
+            case 7:
+                Equation1.setImageResource(R.drawable.rotationalequation2f);
+                break;
+            case 8:
+                Equation1.setImageResource(R.drawable.rotationalequation2i);
+                break;
+            case 9:
+                Equation1.setImageResource(R.drawable.rotationalequation3a);
+                break;
+            case 10:
+                Equation1.setImageResource(R.drawable.rotationalequation3f);
+                break;
+            case 11:
+                Equation1.setImageResource(R.drawable.rotationalequation3i);
+                break;
+            case 12:
+                Equation1.setImageResource(R.drawable.rotationalequation3t);
+                break;
+            case 13:
+                Equation1.setImageResource(R.drawable.rotationalequation4d);
+                break;
+            case 14:
+                Equation1.setImageResource(R.drawable.rotationalequation4f);
+                break;
+            case 15:
+                Equation1.setImageResource(R.drawable.rotationalequation4i);
+                break;
+            case 16:
+                Equation1.setImageResource(R.drawable.rotationalequation4t);
+                break;
+            default:
+                break;
+        }
+
+        switch ((int)secondEquation) {
+            case 1:
+                Equation2.setImageResource(R.drawable.rotationalequation1a);
+                break;
+            case 2:
+                Equation2.setImageResource(R.drawable.rotationalequation1d);
+                break;
+            case 3:
+                Equation2.setImageResource(R.drawable.rotationalequation1i);
+                break;
+            case 4:
+                Equation2.setImageResource(R.drawable.rotationalequation1t);
+                break;
+            case 5:
+                Equation2.setImageResource(R.drawable.rotationalequation2a);
+                break;
+            case 6:
+                Equation2.setImageResource(R.drawable.rotationalequation2d);
+                break;
+            case 7:
+                Equation2.setImageResource(R.drawable.rotationalequation2f);
+                break;
+            case 8:
+                Equation2.setImageResource(R.drawable.rotationalequation2i);
+                break;
+            case 9:
+                Equation2.setImageResource(R.drawable.rotationalequation3a);
+                break;
+            case 10:
+                Equation2.setImageResource(R.drawable.rotationalequation3f);
+                break;
+            case 11:
+                Equation2.setImageResource(R.drawable.rotationalequation3i);
+                break;
+            case 12:
+                Equation2.setImageResource(R.drawable.rotationalequation3t);
+                break;
+            case 13:
+                Equation2.setImageResource(R.drawable.rotationalequation4d);
+                break;
+            case 14:
+                Equation2.setImageResource(R.drawable.rotationalequation4f);
+                break;
+            case 15:
+                Equation2.setImageResource(R.drawable.rotationalequation4i);
+                break;
+            case 16:
+                Equation2.setImageResource(R.drawable.rotationalequation4t);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_rotational_solutions, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
